5d6381d749c9838d870a1283bfb07136e81192c2
Jeremy Eramian <jeremyleramian@outlook.com>
2016-04-08 17:32:01 -0700

    hellp[

--- scripts/components/Header.js
+++ scripts/components/Header.js
@@ -4,7 +4,6 @@ export default class Header extends React.Component
 {
     render()
     {
-        const name ="Jeremy";
         return(
             <header>Header</header>
         );
--- scripts/components/Layout.js
+++ scripts/components/Layout.js
@@ -1,14 +1,16 @@
 import React from "react";
+
+import Footer from "./Footer";
 import Header from "./Header";
 
 export default class Layout extends React.Component
 {
     render()
     {
-        const name ="Jeremy";
         return(
             <div>
                 <Header/>
+                <Footer/>
             </div>
         );
     }
