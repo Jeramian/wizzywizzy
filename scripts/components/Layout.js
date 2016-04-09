f9774906777dfe586e5d2e10d78d31b3870faab9
Jeremy Eramian <jeremyleramian@outlook.com>
2016-04-08 19:13:41 -0700

    starting app

--- scripts/components/Layout.js
+++ scripts/components/Layout.js
@@ -1,19 +1,12 @@
 import React from "react";
 
-import Body from "./Body";
-import Footer from "./Footer";
-import Header from "./Header";
 
 export default class Layout extends React.Component
 {
     render()
     {
         return(
-            <div>
-                <Header />
-                <Body/>
-                <Footer/>
-            </div>
+            <div></div>
         );
     }
 }
